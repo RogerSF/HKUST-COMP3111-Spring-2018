d956510bfc8310b75cfe776170d10ed606bd364e
RogerSF <shaorz@gmail.com>
2018-02-05 23:04:54 +0800

    Second output

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -3,6 +3,8 @@ package labs.comp3111.ust.hk;
 public class Lab1 {
 	public static void main(String[] args) {
 		System.out.println("Lab 1: Introduction to Git and GitHub ");
+		System.out.println("Congratulations! You've learned the Git Basics");
+
 	}
 }
 
